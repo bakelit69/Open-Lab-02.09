c5dba26b2c79ce1b93edca90483b4aa6ac736d59
bakelit69 <113536297+bakelit69@users.noreply.github.com>
2022-09-27 09:53:16 +0200

    Update Names.cs

--- Open-Lab-02.09/Names.cs
+++ Open-Lab-02.09/Names.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._09
 {
@@ -6,7 +6,8 @@ namespace Open_Lab_02._09
     {
         public string ConcatName(string firstName, string lastName)
         {
-            throw new NotImplementedException();
+            string result = lastName +", "+ firstName;
+            return result;
         }
     }
 }
